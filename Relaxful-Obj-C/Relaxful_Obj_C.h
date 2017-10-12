2928b89d32a633ef94fb508651b9905f59fd0429
Ian Foose <ianfoose@me.com>
2017-10-12 00:43:24 -0400

    Initial Commit

---
+++ Relaxful-Obj-C/Relaxful_Obj_C.h
@@ -0,0 +1,19 @@
+//
+//  Relaxful_Obj_C.h
+//  Relaxful-Obj-C
+//
+//  Created by Ian Foose on 10/12/17.
+//  Copyright © 2017 Foose Industries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Relaxful_Obj_C.
+FOUNDATION_EXPORT double Relaxful_Obj_CVersionNumber;
+
+//! Project version string for Relaxful_Obj_C.
+FOUNDATION_EXPORT const unsigned char Relaxful_Obj_CVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Relaxful_Obj_C/PublicHeader.h>
+
+
